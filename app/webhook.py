6eac9d8a4674eb587bc8ac28952b70b8af77c7a4
ligade <sourabhligade07@gmail.com>
2025-12-16 10:13:19 +0530

    ff

--- app/webhook.py
+++ app/webhook.py
@@ -2,6 +2,10 @@ from fastapi import FastAPI, Header, Request
 import hmac, hashlib, json, os
 from fastapi.responses import FileResponse
 from pathlib import Path
+from fastapi import Request, Response
+from fastapi.responses import StreamingResponse
+import math
+
 
 app = FastAPI()
 
@@ -24,11 +28,45 @@ BASE_DIR = Path(__file__).resolve().parent
 VIDEO_PATH = BASE_DIR / "out.mp4"
 
 # Serve video
+
 @app.get("/out.mp4")
-def get_video():
-    if not VIDEO_PATH.exists():
+def get_video(request: Request):
+    file_path = VIDEO_PATH
+    if not file_path.exists():
         return {"error": "Video not generated yet"}
-    return FileResponse(VIDEO_PATH, media_type="video/mp4")
+
+    file_size = file_path.stat().st_size
+    range_header = request.headers.get("range")
+    start = 0
+    end = file_size - 1
+
+    if range_header:
+        bytes_range = range_header.replace("bytes=", "").split("-")
+        if bytes_range[0]:
+            start = int(bytes_range[0])
+        if bytes_range[1]:
+            end = int(bytes_range[1])
+    length = end - start + 1
+
+    def iterfile(path, start, length):
+        with open(path, "rb") as f:
+            f.seek(start)
+            remaining = length
+            while remaining > 0:
+                chunk_size = min(1024*1024, remaining)
+                data = f.read(chunk_size)
+                if not data:
+                    break
+                remaining -= len(data)
+                yield data
+
+    headers = {
+        "Content-Range": f"bytes {start}-{end}/{file_size}",
+        "Accept-Ranges": "bytes",
+        "Content-Length": str(length),
+        "Content-Type": "video/mp4",
+    }
+    return StreamingResponse(iterfile(file_path, start, length), status_code=206, headers=headers)
 
 # Verify GitHub signature
 def verify_signature(signature, payload):
