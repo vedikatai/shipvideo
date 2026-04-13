2be651b556025042c19a7364695b3fa2f1deefa5
ligade <sourabhligade07@gmail.com>
2026-04-13 23:27:10 +0530

    hh

--- cleanup_r2.py
+++ cleanup_r2.py
@@ -1,4 +1,4 @@
-#!/usr/bsssin/ssesssssssssssssssnv python3
+#!/usr/bsssin/sshjhhesssssssssssssssnv python3
 """
 Manual R2 cleanup script.
 Usage: python3 cleanup_r2.py [--max-videos N] [--max-age-days N]
